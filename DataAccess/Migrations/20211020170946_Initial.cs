bc7429be59f02e3773bd6b3cc3f1bae53750b485
Lusine Yenokyan <lusine.yenokyan@acba.am>
2021-10-20 21:11:09 +0400

    DatebaseMigratinAdd

---
+++ DataAccess/Migrations/20211020170946_Initial.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace DataAccess.Migrations
+{
+    public partial class Initial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Banks",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BankName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    BankURL = table.Column<string>(type: "nvarchar(500)", maxLength: 500, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Banks", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BotHistory",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ChatId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    CommandText = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Reply = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    UserId = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BotHistory", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Currencies",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Code = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Currencies", x => x.ID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Rates",
+                columns: table => new
+                {
+                    ID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    FromCurrency = table.Column<string>(type: "nvarchar(3)", maxLength: 3, nullable: false),
+                    ToCurrency = table.Column<string>(type: "nvarchar(3)", maxLength: 3, nullable: false),
+                    BuyValue = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    SellValue = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    BankId = table.Column<int>(type: "int", nullable: false),
+                    LastUpdated = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Rates", x => x.ID);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rates_FromCurrency",
+                table: "Rates",
+                column: "FromCurrency");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Rates_ToCurrency",
+                table: "Rates",
+                column: "ToCurrency");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Banks");
+
+            migrationBuilder.DropTable(
+                name: "BotHistory");
+
+            migrationBuilder.DropTable(
+                name: "Currencies");
+
+            migrationBuilder.DropTable(
+                name: "Rates");
+        }
+    }
+}
