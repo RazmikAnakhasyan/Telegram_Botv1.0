2070b78d8783fbc7957d564b1be062443398b551
Gegham775 <hunanyan.gegam7@gmail.com>
2021-10-20 21:05:40 +0400

    Config file added

--- TelegramBot/TelegramCommandHandler.cs
+++ TelegramBot/TelegramCommandHandler.cs
@@ -1,15 +1,23 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
 using Telegram.Bot;
 
 namespace TelegramBot
 {
     public class TelegramCommandHandler
     {
-        private static string _token = "2073841951:AAEwv5BcSBbG3X1VGyzkV-0dtjrCTVBWxqk";
+        private readonly string _token;
+        private readonly TelegramBotClient Bot;
+        public TelegramCommandHandler()
+        {
+            Settings settings = JsonConvert.DeserializeObject<Settings>(File.ReadAllText("settings.json"));
+            _token = settings.Token;
+            Bot = new TelegramBotClient(_token);
+        }
 
-        private static readonly TelegramBotClient Bot = new TelegramBotClient(_token);
 
-        private static void Bot_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
+        private void Bot_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e)
         {
             if (e.Message.Type == Telegram.Bot.Types.Enums.MessageType.Text)
             {
