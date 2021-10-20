de19ff8b74993e6a5f0c724a6642f5a72a896022
AlexandrSargsyan <alexandrsargsyan1@gmail.com>
2021-10-20 19:57:46 +0400

    Some fixes

---
+++ Telegram_Bot_API/Controller/TestController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controller
+{
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        [HttpGet("test")]
+        public string Get()
+        {
+            return "Tech42 .NET Currency API";
+        }
+    }
+}
