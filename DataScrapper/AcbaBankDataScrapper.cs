f48f23edcc9e5607b5d64e52bdbdcc486425d8e3
AlexandrSargsyan <alexandrsargsyan1@gmail.com>
2021-11-04 13:57:25 +0400

    --ACBA fixed

--- DataScrapper/AcbaBankDataScrapper.cs
+++ DataScrapper/AcbaBankDataScrapper.cs
@@ -12,26 +12,26 @@ namespace htmlWrapDemo
         public IEnumerable<CurrencyModel> Get()
         {
             var client = new HttpClient();
-            var html = client.GetStringAsync("https://www.acba.am")
+            var html = client.GetStringAsync("https://www.acba.am/en")
                 .GetAwaiter()
                 .GetResult();
 
-            //var html = File.ReadAllText("test.html");
             var doc = new HtmlDocument();
             doc.LoadHtml(html);
-            var nodes = doc.DocumentNode.SelectNodes("/html/body/div/div/main/div[3]/div[1]/div/div[2]/div[2]/div/div[2]/div/div[2]").ToArray();
+            var nodes = doc.DocumentNode.SelectNodes("//div[@class='simple_price-row']").Skip(1);
+                
             List<CurrencyModel> models = new List<CurrencyModel>();
             foreach (var item in nodes)
             {
-                var tds = item.SelectNodes("div").Take(3).ToArray();
+                var childs = item.ChildNodes.Where(_ => _.Name == "div").ToArray();
 
                 try
                 {
 
                     var model = new CurrencyModel();
-                    model.Currency = tds[0].InnerText.Replace("\n","").Replace("\t","");
-                    model.BuyValue = decimal.Parse(tds[1].InnerText);
-                    model.SellValue = decimal.Parse(tds[2].InnerText);
+                    model.Currency = childs[0].InnerText.Trim();
+                    model.BuyValue = decimal.Parse(childs[1].InnerText.Trim());
+                    model.SellValue = decimal.Parse(childs[2].InnerText.Trim());
 
                     models.Add(model);
                 }
