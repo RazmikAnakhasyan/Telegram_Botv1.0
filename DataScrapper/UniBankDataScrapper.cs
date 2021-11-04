1a723a5c7df189bef85b9bbb1fe3af289c851598
AlexandrSargsyan <alexandrsargsyan1@gmail.com>
2021-11-04 15:39:01 +0400

    --evoca & unibank fixed

--- DataScrapper/UniBankDataScrapper.cs
+++ DataScrapper/UniBankDataScrapper.cs
@@ -19,25 +19,21 @@ namespace htmlWrapDemo
             //var html = File.ReadAllText("test.html");
             var doc = new HtmlDocument();
             doc.LoadHtml(html);
-            var nodes = doc.DocumentNode.SelectNodes("/html/body/div/div[2]/section[1]/div/div/div/div[2]/div[1]/div[1]/ul[2]").ToArray();
+            var nodes = doc.DocumentNode.SelectNodes("/html/body/div/div[2]/section[1]/div/div/div/div[2]/div[1]/div[1]/ul[2]/li")
+                .AsEnumerable();
             List<CurrencyModel> models = new List<CurrencyModel>();
-            foreach (var item in nodes)
+            while (nodes.GetEnumerator().MoveNext())
             {
-                var tds = item.SelectNodes("li").ToArray();
-                    try
-                    {
-
-                        var model = new CurrencyModel();
-                        model.Currency = tds[0].InnerText;
-                        model.BuyValue = decimal.Parse(tds[1].InnerText);
-                        model.SellValue = decimal.Parse(tds[2].InnerText);
-
-                        models.Add(model);
-                    }
-                    catch { }
+                var model = new CurrencyModel();
                 
+                model.Currency = nodes.ElementAt(0).InnerText.Trim();
+                model.BuyValue = decimal.Parse(nodes.ElementAt(1).InnerText.Trim());
+                model.SellValue = decimal.Parse(nodes.ElementAt(2).InnerText.Trim());
 
+                models.Add(model);
+                nodes = nodes.Skip(3);
             }
+         
             return models;
         }
     
